a75fb8fc8b2eeed1e86369f6be4791a7845eebb8
it99plus <it99plus@gmail.com>
2014-11-23 22:31:02 +0000

    ApplicationContext context = new FileSystemXmlApplicationContext('beans.xml')

---
+++ testsprog/src/main/java/com/it99plus/spring/test/testsprog/App.java
@@ -0,0 +1,13 @@
+package com.it99plus.spring.test.testsprog;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
