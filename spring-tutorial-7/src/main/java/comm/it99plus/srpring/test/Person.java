1ec7511178062cba8c2460bf4b500e41c4b7057c
it99plus <it99plus@gmail.com>
2014-11-23 23:33:24 +0000

    constructor-arguments inside bean.xml and eclipse editor

---
+++ spring-tutorial-7/src/main/java/comm/it99plus/srpring/test/Person.java
@@ -0,0 +1,26 @@
+package comm.it99plus.srpring.test;
+
+public class Person {
+
+	private int id;
+	private String name;
+	
+	public Person() {}
+	
+	public Person(int id, String name) {
+		this.id = id;
+		this.name = name;
+	}
+
+	public void speak() {
+		System.out.println("Hello I am a Person");
+	}
+
+	@Override
+	public String toString() {
+		return "Person [id=" + id + ", name=" + name + "]";
+	}
+	
+	
+	
+}
