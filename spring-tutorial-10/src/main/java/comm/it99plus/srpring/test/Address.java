5feaba3eb631d293712c457e79b91caf53716877
it99plus <it99plus@gmail.com>
2014-11-24 00:31:48 +0000

    example demonstrating a sigleton scope - which is the defaul lt scopscope

---
+++ spring-tutorial-10/src/main/java/comm/it99plus/srpring/test/Address.java
@@ -0,0 +1,20 @@
+package comm.it99plus.srpring.test;
+
+public class Address {
+
+	private String street;
+	private String postcode;
+
+	public Address() {}
+	
+	public Address(String street, String postcode) {
+		this.street = street;
+		this.postcode = postcode;
+	}
+
+	@Override
+	public String toString() {
+		return "Address [street=" + street + ", psotcode=" + postcode + "]";
+	}
+
+}
---
+++ spring-tutorial-10/src/main/java/comm/it99plus/srpring/test/App.java
@@ -0,0 +1,25 @@
+package comm.it99plus.srpring.test;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class App {
+
+	public static void main(String[] args) {
+
+		// to load my bean.xml from any folder on the Classpath
+		ApplicationContext context = new ClassPathXmlApplicationContext("com/it99plus/spring/test/beans/beans.xml");  
+		
+		
+		Person person1 =  (Person) context.getBean("person");
+		Person person2 =  (Person) context.getBean("person");
+		
+		person1.setTaxId(666);
+		
+		System.out.println(person2);
+		
+		
+		((ClassPathXmlApplicationContext)context).close(); 
+	}
+
+}
