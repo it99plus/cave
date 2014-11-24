b2e1b695b394e022b821c13b8644e8e778bfd54d
it99plus <it99plus@gmail.com>
2014-11-24 01:05:50 +0000

    video 12 - demonstrating factroy-method getInstace() with and without args

---
+++ spring-tutorial-12/src/main/java/comm/it99plus/srpring/test/App.java
@@ -0,0 +1,23 @@
+package comm.it99plus.srpring.test;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class App {
+
+	public static void main(String[] args) {
+
+		// to load my bean.xml from any folder on the Classpath
+		ApplicationContext context = new ClassPathXmlApplicationContext(
+				"com/it99plus/spring/test/beans/beans.xml");
+
+		Person person = (Person) context.getBean("person");
+
+		person.setTaxId(666);
+
+		System.out.println(person);
+
+		((ClassPathXmlApplicationContext) context).close();
+	}
+
+}
