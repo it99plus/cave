f3b7bedbf16718947581b8b975e09efafc1d70fe
it99plus <it99plus@gmail.com>
2014-11-24 00:19:34 +0000

    Dpendency injection - injecting Address int Person using setters and brs and beans.xml

---
+++ spring-tutorial-9/src/main/java/comm/it99plus/srpring/test/App.java
@@ -0,0 +1,24 @@
+package comm.it99plus.srpring.test;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class App {
+
+	public static void main(String[] args) {
+
+		// to load my bean.xml from any folder on the Classpath
+		ApplicationContext context = new ClassPathXmlApplicationContext("com/it99plus/spring/test/beans/beans.xml");  
+		
+		Person person =  (Person) context.getBean("person");
+		
+		System.out.println(person);
+		person.speak();
+		
+		Address address = (Address) context.getBean("address");
+		System.out.println(address);
+		
+		((ClassPathXmlApplicationContext)context).close(); 
+	}
+
+}
