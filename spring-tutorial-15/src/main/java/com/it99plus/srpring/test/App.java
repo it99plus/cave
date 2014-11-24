0c2966f7d3e44db00a677fb2bebdd6992830d2c2
it99plus <it99plus@gmail.com>
2014-11-24 12:05:43 +0000

    video 15 - List of Beans configured in beans.xml - Jungle/animals

---
+++ spring-tutorial-15/src/main/java/com/it99plus/srpring/test/App.java
@@ -0,0 +1,20 @@
+package com.it99plus.srpring.test;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class App {
+
+	public static void main(String[] args) {
+
+		ApplicationContext context = new ClassPathXmlApplicationContext(
+				"com/it99plus/spring/test/beans/beans.xml");
+
+		Jungle jungle = (Jungle) context.getBean("jungle");
+
+		System.out.println(jungle);
+
+		((ClassPathXmlApplicationContext) context).close();
+	}
+
+}
