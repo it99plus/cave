162c4756608f4c0667e60e6cf27ee0a566da80fd
it99plus <it99plus@gmail.com>
2014-11-25 21:58:32 +0000

    Video 29 - @Request annotation - JSR-250 ANNOTATIONS

---
+++ spring-tutorial-28/src/main/java/com/caveofprogramming/spring/test/Logger.java
@@ -0,0 +1,37 @@
+package com.caveofprogramming.spring.test;
+
+import javax.annotation.Resource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+
+/*
+ * Dummy implementation of logger.
+ */
+
+public class Logger {
+
+	private ConsoleWriter consoleWriter;
+	private LogWriter fileWriter;
+	
+	
+	@Resource
+	public void setConsoleWriter(ConsoleWriter writer) {
+		this.consoleWriter = writer;
+	}
+
+	@Resource(name="lkjhkhlkhkhlkh")
+	public void setFileWriter(LogWriter fileWriter) {
+		this.fileWriter = fileWriter;
+	}
+	
+	public void writeFile(String text) {  
+		fileWriter.write(text);
+	}
+	
+	public void writeConsole(String text) {
+		if (consoleWriter != null)
+		    consoleWriter.write(text);
+	}
+
+}
